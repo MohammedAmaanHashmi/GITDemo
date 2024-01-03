a68e615a7fea6a4967e6d24ce799d9667a8447c5
Amaan <amaanhashmi98@gmail.com>
2024-01-03 12:32:41 +0530

    American guy commit

--- src/BasicAuthentication.java
+++ src/BasicAuthentication.java
@@ -1,30 +1,37 @@
 import org.openqa.selenium.HasAuthentication;
 import org.openqa.selenium.UsernameAndPassword;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.net.URI;
 import java.util.function.Predicate;
 
 public class BasicAuthentication {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-		
-		System.setProperty("webdriver.chrome.driver", "/Users/rahulshetty/Documents/chromedriver");
-		System.setProperty("webdriver.ie.driver", "/Users/rahulshetty/Documents/chromedriver");
-		
-		ChromeDriver driver = new ChromeDriver();
-		ChromeDriver driver1 = new ChromeDriver();
 
-		//predicate, consumer, 0 examples
-		Predicate<URI> uriPredicate =uri ->  uri.getHost().contains("httpbin.org");
-		
-		((HasAuthentication)driver).register(uriPredicate,UsernameAndPassword.of("foo", "bar"));
-		driver.get("http://httpbin.org/basic-auth/foo/bar");
-		driver.get("http://httpbin.org/basic-auth/foo/jar");
-		driver.get("http://httpbin.org/basic-auth/foo/tar");
-		   
-		
-	}
+        System.setProperty("webdriver.chrome.driver", "/Users/rahulshetty/Documents/chromedriver");
+        System.setProperty("webdriver.ie.driver", "/Users/rahulshetty/Documents/chromedriver");
+
+        ChromeDriver driver = new ChromeDriver();
+        ChromeDriver driver1 = new ChromeDriver();
+
+        //predicate, consumer, 0 examples
+        Predicate<URI> uriPredicate = uri -> uri.getHost().contains("httpbin.org");
+
+        ((HasAuthentication) driver).register(uriPredicate, UsernameAndPassword.of("foo", "bar"));
+        driver.get("http://httpbin.org/basic-auth/foo/bar");
+        driver.get("http://httpbin.org/basic-auth/foo/jar");
+        driver.get("http://httpbin.org/basic-auth/foo/tar");
+        //free
+        //freee
+		//freee
+		//f
+		System.out.println("Hello World");
+		System.out.println("Hello Universe");
+		System.out.println("Hello Amaan");
+
+    }
 
 }
